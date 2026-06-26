d50f7f0df5d959a65e49f49ed09eb5d6d8705cac
Maspur102 <maspur15022005@gmail.com>
2026-06-26 12:27:35 +0700

    fix : versi baru flunter

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,10 +12,33 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// --- SUNTIKAN KODE PENYELAMAT UNTUK ISAR (NAMESPACE FIX) ---
+// Blok ini akan mengecek setiap package pihak ketiga. Jika package
+// tersebut tidak memiliki namespace, kita akan paksakan mengambil dari group-nya.
+subprojects {
+    afterEvaluate {
+        val androidExt = extensions.findByName("android")
+        if (androidExt != null) {
+            try {
+                // Menggunakan reflection (kaca pantul) untuk membaca & menulis namespace secara aman
+                val getNamespaceMethod = androidExt.javaClass.getMethod("getNamespace")
+                val currentNamespace = getNamespaceMethod.invoke(androidExt)
+                if (currentNamespace == null) {
+                    val setNamespaceMethod = androidExt.javaClass.getMethod("setNamespace", String::class.java)
+                    setNamespaceMethod.invoke(androidExt, project.group.toString())
+                }
+            } catch (e: Exception) {
+                // Abaikan dengan aman jika package tersebut tidak relevan
+            }
+        }
+    }
 }
