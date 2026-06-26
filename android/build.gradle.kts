b93020263dad183dbc1e1f67b257f6020c8110e8
Maspur102 <maspur15022005@gmail.com>
2026-06-26 13:16:13 +0700

    fix: force compileSdk 34 on all subprojects to resolve lStar error

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,11 +13,13 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
-// Penyelamat Namespace untuk package Isar
+// --- SUNTIKAN KODE PENYELAMAT SUPER (NAMESPACE & LSTAR FIX) ---
 subprojects {
     afterEvaluate {
         val androidExt = extensions.findByName("android")
         if (androidExt != null) {
+            
+            // 1. Penyelamat Namespace Isar (Mencegah Error Pertama)
             try {
                 val getNamespaceMethod = androidExt.javaClass.getMethod("getNamespace")
                 val currentNamespace = getNamespaceMethod.invoke(androidExt)
@@ -26,6 +28,18 @@ subprojects {
                     setNamespaceMethod.invoke(androidExt, project.group.toString())
                 }
             } catch (e: Exception) { }
+
+            // 2. Penyelamat lStar (Mencegah Error Kedua: Memaksa semua package ke SDK 34)
+            try {
+                val setCompileSdkMethod = androidExt.javaClass.getMethod("setCompileSdk", Int::class.java)
+                setCompileSdkMethod.invoke(androidExt, 34)
+            } catch (e: Exception) {
+                try {
+                    val compileSdkVersionMethod = androidExt.javaClass.getMethod("compileSdkVersion", Int::class.java)
+                    compileSdkVersionMethod.invoke(androidExt, 34)
+                } catch (e2: Exception) { }
+            }
+            
         }
     }
 }
